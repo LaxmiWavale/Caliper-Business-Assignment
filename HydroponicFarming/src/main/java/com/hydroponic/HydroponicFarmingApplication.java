cd95c685c01e2dac46c18332aed6e832f8351a06
LaxmiWavale <laxmiwavale1995@gmail.com>
2022-11-09 16:39:55 +0530

    project initialised

---
+++ HydroponicFarming/src/main/java/com/hydroponic/HydroponicFarmingApplication.java
@@ -0,0 +1,13 @@
+package com.hydroponic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroponicFarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydroponicFarmingApplication.class, args);
+	}
+
+}
---
+++ HydroponicFarming/src/test/java/com/hydroponic/HydroponicFarmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydroponic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydroponicFarmingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
